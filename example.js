0637eab83d2bd5e2f820b46888dcf7ecf3a2b441
Tristan Davies <github@tristan.io>
2013-03-12 17:10:44 -0400

    Added a new example

---
+++ example.js
@@ -0,0 +1,22 @@
+var meddle = require('./index.js');
+
+var sendMessage = meddle('send', function(data, next){
+	console.log('SEND:', data.text);
+	next();
+});
+
+var sender = meddle();
+sender.use(sendMessage);
+
+var send = sender('send');
+
+var emotes = meddle('emotes', function(data, next){
+	data.text = data.text.split(':)').join('<img src="smiley.gif"/>');
+	next();
+}).before('send');
+
+send({text: 'Hi! :)'});
+
+sender.use(emotes);
+
+send({text: 'Hi! :)'});
